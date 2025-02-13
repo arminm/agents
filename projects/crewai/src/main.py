76a22ea4fe9fcaad001e36f6d5e84d30c361614b
Armin Mahmoudi <arminm89@gmail.com>
2025-02-12 23:05:10 -0800

    add analysis example

--- projects/crewai/src/main.py
+++ projects/crewai/src/main.py
@@ -4,6 +4,9 @@ import yaml
 from pathlib import Path
 
 from crewai import Agent, Crew, Task
+from crewai_tools import CSVSearchTool, FileReadTool
+
+
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 
@@ -16,10 +19,13 @@ llm = ChatGroq(
     api_key=os.getenv("GROQ_API_KEY"),
     max_retries=2,
     timeout=10,
+    max_tokens=8192,
+    # rate_limiter=1000,
 )
 
 # Define file paths for YAML configurations
-config_path = 'src/config/agents_and_tasks.yaml'
+# config_path = 'src/config/agents_and_tasks.yaml'
+config_path = 'src/config/mass_spec_agents_and_tasks.yaml'
 configs = {}
 
 # Load configurations from YAML files
@@ -29,6 +35,55 @@ with open(config_path, 'r') as file:
 # Assign loaded configurations to specific variables
 agents_config = configs['agents']
 tasks_config = configs['tasks']
+
+def analyze_data():
+    # Initialize agents
+    file_read_tool = FileReadTool(file_path='src/config/combined_protein.csv')
+    csv_read_tool = CSVSearchTool(csv='src/config/combined_protein.csv')
+
+    data_analyst = Agent(
+        config=agents_config['data-analyst'],
+        # llm=llm,
+        tools=[file_read_tool, csv_read_tool],
+        # max_tokens=12800
+    )
+    
+    mass_spec_expert = Agent(
+        config=agents_config['mass-spec-expert'],
+        # tools=[file_read_tool],
+        # max_tokens=12800
+        # llm=llm,
+    )
+    
+    bioinformatician = Agent(
+        config=agents_config['bioinformatician'],
+        # max_tokens=12800
+        # llm=llm,
+    )
+
+    # Define tasks
+    data_processing_task = Task(
+        config=tasks_config['data_processing'],
+        agent=data_analyst,
+    )
+
+    diff_analysis_task = Task(
+        config=tasks_config['differential_analysis'],
+        agent=bioinformatician,
+        output_file='output/analysis.md'
+    )
+
+    # Create crew
+    analysis_crew = Crew(
+        agents=[data_analyst, mass_spec_expert, bioinformatician],
+        tasks=[data_processing_task, diff_analysis_task],
+        verbose=True,
+    )
+
+    # Kick off the crew's work
+    analysis_crew.kickoff({
+    })
+
 
 
 def create_content(topic):
@@ -94,6 +149,7 @@ def main():
     parser.add_argument("--topic", type=str, help="Topic to research and write about")
     parser.add_argument('--publish', action='store_true', help='Publish the content')
     parser.add_argument('--draft-id', type=str, help='Draft ID to publish')
+    parser.add_argument('--analyze', action='store_true',, help='Analyze Data')
 
     args = parser.parse_args()
 
@@ -104,6 +160,8 @@ def main():
         print("Publishing content is not yet implemented.")
         # result = asyncio.run(publish_content(args.draft_id))
         # print(f"Publishing results: {result}")
+    elif args.analyze:
+        analyze_data()
     else:
         parser.print_help()
 
